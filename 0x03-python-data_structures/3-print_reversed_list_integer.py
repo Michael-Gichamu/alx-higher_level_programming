6040f3dead4441991cf33cc2574d047e46728c20
Michael_Gichamu <michaelmatere606@gmail.com>
2023-03-13 14:50:20 -0700

    Function that removes all characters c and C from a string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    len_list = (len(my_list) - 1)
-    while len_list >= 0:
-        print("{:d}".format(my_list[len_list]))
+    while (len(my_list) - 1) >= 0:
+        print("{:d}".format(my_list[(len(my_list) - 1)]))
         len_list -= 1
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = my_string.replace("C", "").replace("c", "")
+    return new_string
