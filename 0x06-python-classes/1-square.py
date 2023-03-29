8a65418cd79440c8d4ecf668531591b11c467bff
Michael-Gichamu <michaelmatere606@gmail.com>
2023-03-29 23:27:29 +0300

    Square with size: defines a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """
+    class square that has attributes:
+       size
+    """
+    def __init__(self, size):
+        """
+        Initialization method for square class
+        """
+        self.__size = size
