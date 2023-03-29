af4388adb641bf398e5fc5924959f747edc80d6f
Michael-Gichamu <michaelmatere606@gmail.com>
2023-03-29 15:23:05 -0700

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         """Initialize a new square.
 
         Args:
@@ -15,7 +15,6 @@ class Square:
             tuple of 2 non-negative integers.
         """
         self.size = size
-        self.position = position
 
     @property
     def size(self):
@@ -41,28 +40,6 @@ class Square:
         else:
             self.__size = value
 
-    @property
-    def position(self):
-        """Get the current position of the square."""
-        return (self.__position)
-
-    @position.setter
-    def position(self, value):
-        """Set the position of the square.
-
-        Args:
-            value (tuple): The new position of the square
-            as a tuple of 2 positive integers.
-        Raises:
-            TypeError: If `value` is not a tuple of 2 positive integers.
-        """
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
     def area(self):
         """Calculate the area of the square.
 
@@ -73,12 +50,8 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
             print("")
