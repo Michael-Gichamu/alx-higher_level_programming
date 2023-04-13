934ed230b4d16d2eec88b8c8e70886e60709f594
Michael-Gichamu <michaelmatere606@gmail.com>
2023-04-13 14:17:24 -0700

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines a class MyInt that inherits from int.
+"""
+
+
+class MyInt(int):
+    """
+    Inverts int operators.
+    === and !=.
+
+    """
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
