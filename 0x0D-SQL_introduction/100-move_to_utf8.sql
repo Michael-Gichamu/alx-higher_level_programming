7adca2e8238940fd3d12159917a4195db50d23c5
Michael-Gichamu <michaelmatere606@gmail.com>
2023-05-16 21:50:58 +0000

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
--- Convert the database.
+-- USE Database
 USE `hbtn_0c_0`
+-- Convert the database to UTF8
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert the table to UTF8
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
