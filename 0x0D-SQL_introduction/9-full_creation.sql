fbae762dfeeb7b9fb99e28f23bfb5689c96b2ab4
Michael-Gichamu <michaelmatere606@gmail.com>
2023-05-16 21:52:24 +0000

    Full Creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,11 @@ CREATE TABLE IF NOT EXISTS `second_table`(
     `id` INT,
     `name` VARCHAR(256),
     `score` INT);
+-- Insert value to second table.
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, 'John', 10);
+-- Insert value to second table.
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, 'ALex', 3);
+-- Insert value to second table.
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, 'Bob', 14);
+-- Insert value to second table.
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4, 'George', 8);
