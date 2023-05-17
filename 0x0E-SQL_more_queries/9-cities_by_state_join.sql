8348538dabdb8d52bcbee2c7884ac716cc55d73b
Michael-Gichamu <michaelmatere606@gmail.com>
2023-05-17 20:30:19 +0000

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- List all cities in the database hbtn_0d_usa.
--- records sorted in ascending order by cities.id.
-SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
-FROM `cities`
-    INNER JOIN `states` ON `cities`.`id` = `states`.`id`
-ORDER BY `cities`.`id`;
+SELECT city.`id`, city.`name`, state.`name`
+  FROM `cities` AS city
+       INNER JOIN `states` AS state
+       ON city.`state_id` = state.`id`
+ ORDER BY city.`id`;i
