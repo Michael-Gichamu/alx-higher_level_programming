ad2178256833caf43591b55495bbecff7a09b3b4
Michael-Gichamu <michaelmatere606@gmail.com>
2023-06-12 16:36:21 +0000

    An integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const parsedInt = parseInt(process.argv[2]);
+
+if (Number.isNaN(parsedInt)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parsedInt}`);
+}
+
