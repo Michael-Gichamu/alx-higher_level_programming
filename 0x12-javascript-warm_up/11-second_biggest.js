32c9120108ae99ebfeb3e33637b35615a6184f52
Michael-Gichamu <michaelmatere606@gmail.com>
2023-06-12 18:53:15 +0000

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv;
+if (typeof args[2] === 'undefined' || args.length === 3) {
+  console.log(0);
+} else {
+  let temp;
+  for (let i = 0; i < (args.length - 1); i++) {
+    for (let i = 0; i < (args.length - 1); i++) {
+      temp = args[i];
+      if (temp > args[i + 1]) {
+        args[i] = args[i + 1];
+        args[i + 1] = temp;
+      }
+    }
+  }
+  console.log(args[(args.length - 2)]);
+}
