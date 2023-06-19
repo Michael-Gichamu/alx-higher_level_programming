a3ff0f1454b3053d38a93ade554fa6d013d064c7
Michael-Gichamu <michaelmatere606@gmail.com>
2023-06-19 05:08:33 +0000

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Adds State object "Louisiana" to the database hbtn_0e_6_usa
+Usage: ./11-model_state_insert.py <mysql username> \
+                                  <mysql password> \
+                                  <mysql database name> \
+"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        pool_pre_ping=True
+        )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
