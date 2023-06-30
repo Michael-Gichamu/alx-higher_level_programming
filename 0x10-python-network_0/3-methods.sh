2e2ffc17ec0431935eaaf2fb2ef810f8b6c6d5f0
Michael <115511086+Michael-Gichamu@users.noreply.github.com>
2023-06-30 15:38:12 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept
-headers=$(curl -s -I "$1")
-allowed_methods=$(echo "$headers" | awk -F ': ' '/^Allow:/ {print $2}')
-echo "$allowed_methods"
-
+URL="$1"
+curl -sI "$URL" | grep "Allow" | cut -d " " -f 2-
