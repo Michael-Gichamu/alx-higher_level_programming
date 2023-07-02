ced686ddac24ec01722fbac1ef9ad94a7652767c
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-02 07:36:00 +0000

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Sends a Post request to passed URL with email as parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("utf-8")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as resp:
+        print(resp.read())
