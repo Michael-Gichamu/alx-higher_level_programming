353c52ceb0db3ad94d017e57b01732950b432d80
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-02 14:13:52 +0000

    Say Hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+(function () {
+  $.ajax({
+    url: "https://fourtonfish.com/hellosalut/?lang=fr",
+    method: "GET",
+    success: function (data) {
+      $('DIV#hello').text(data.hello);
+    }
+  });
+})();
